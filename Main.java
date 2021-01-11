4ddc798f44154072def91a66c25ae60f2c888827
Emiliano Melendrez <91090413@g.mvusd.net>
2021-01-11 20:12:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner (System.in);
+    int num1;
+    System.out.println("Pick an int: ");
+    num1 = kb.nextInt();
+    System.out.println("Todays Lucky Number is " + num1);
+  }
+}
